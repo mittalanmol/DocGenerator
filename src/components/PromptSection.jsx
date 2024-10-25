b4c9c8d048dcdad1e1eadc930a75ea06ce707ffe
Anmol Mittal <anmol0711mittal@gmail.com>
2024-10-25 14:42:36 +0530

    add handle state in prompt component

--- src/components/PromptSection.jsx
+++ src/components/PromptSection.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import "./PromptSection.css";
 
 const PromptSection = () => {
+  const [inputText, setInputText] = useState("");
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = () => {};
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 flex-column">
-      <textarea className=" prompt" placeholder="Enter the instructions..." />
-      <button className="generate-btn mt-4">Generate</button>
+    <div className='d-flex justify-content-center align-items-center vh-100 flex-column'>
+      <textarea
+        className=' prompt'
+        placeholder='Enter the instructions...'
+        onChange={handleChange}
+      />
+      <button className='generate-btn mt-4' onSubmit={handleSubmit}>
+        Generate
+      </button>
     </div>
   );
 };
