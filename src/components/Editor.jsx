e046d3dc60429eeab54e9b1036b8ad2d1170944b
Anmol Mittal <anmol0711mittal@gmail.com>
2024-10-29 15:04:50 +0530

    disable editing on preview div and add necessary plugins in editor

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,37 +1,212 @@
+// import { useRef, useState, useEffect } from "react";
+// import "./Editor.css";
+// import { CKEditor } from "@ckeditor/ckeditor5-react";
+// import {
+//   ClassicEditor,
+//   Essentials,
+//   Autoformat,
+//   BlockQuote,
+//   Bold,
+//   CloudServices,
+//   Code,
+//   CodeBlock,
+//   Heading,
+//   HorizontalLine,
+//   Image,
+//   ImageToolbar,
+//   ImageUpload,
+//   Base64UploadAdapter,
+//   Italic,
+//   Link, // To make Link
+//   List, // Bullet and numbered lists
+//   Markdown,
+//   Mention,
+//   Paragraph,
+//   SourceEditing,
+//   SpecialCharacters,
+//   SpecialCharactersEssentials,
+//   Strikethrough,
+//   Table,
+//   TableToolbar,
+//   TextTransformation,
+//   TodoList,
+// } from "ckeditor5";
+
+// import "ckeditor5/ckeditor5.css";
+// const Editor = ({ message }) => {
+//   const editorRef = useRef(null);
+
+//   const [editorData, setEditorData] = useState(message);
+//   const [data, setData] = useState("");
+
+//   useEffect(() => {
+//     const targetElement = document.getElementById("target-content");
+
+//     // Helper function to update the target content
+//     const updateTargetContent = () => {
+//       const sourceElement =
+//         document.getElementsByClassName("ck ck-editor__main")[0];
+//       if (sourceElement && targetElement) {
+//         targetElement.innerHTML = sourceElement.innerHTML;
+//       }
+//     };
+
+//     // Initial check to wait for the editor to load
+//     const checkEditorLoaded = setInterval(() => {
+//       const sourceElement =
+//         document.getElementsByClassName("ck ck-editor__main")[0];
+//       if (sourceElement) {
+//         clearInterval(checkEditorLoaded); // Stop checking once loaded
+//         updateTargetContent(); // Update with initial content
+//       }
+//     }, 100); // Check every 100ms
+
+//     // Set up MutationObserver to watch for changes in the editor
+//     const observer = new MutationObserver(() => updateTargetContent());
+
+//     // Observe changes if the editor has loaded
+//     const editorContainer =
+//       document.getElementsByClassName("ck ck-editor__main")[0];
+//     if (editorContainer) {
+//       observer.observe(editorContainer, { childList: true, subtree: true });
+//     }
+
+//     // Clean up intervals and observer on unmount
+//     return () => {
+//       clearInterval(checkEditorLoaded);
+//       observer.disconnect();
+//     };
+//   }, [editorData]);
+
+//   return (
+//     <div>
+//       <div className='container'>
+//         <div className='row p-4 '>
+//           <div className='col-md-6 col-12'>
+//             <div className='editor-container-left'>
+//               <CKEditor
+//                 ref={editorRef}
+//                 editor={ClassicEditor}
+//                 data={editorData}
+//                 onChange={(event, editor) => {
+//                   console.log("event", event);
+//                   console.log("editor", editor);
+//                   const data = editor.getData();
+//                   setEditorData(data);
+//                 }}
+//                 config={{
+//                   allowedContent: true,
+//                   extraPlugins: [SourceEditing],
+//                   toolbar: {
+//                     items: [
+//                       "undo",
+//                       "redo",
+//                       "|",
+//                       "bold",
+//                       "italic",
+//                       "SourceEditing",
+//                       "Heading",
+//                       "|",
+//                       "bulletedList", // For bullet points
+//                       "numberedList", // For ordered points
+//                       "indent", // For indentation
+//                     ],
+//                   },
+//                   plugins: [
+//                     ClassicEditor,
+//                     Essentials,
+//                     Autoformat,
+//                     BlockQuote,
+//                     Bold,
+//                     CloudServices,
+//                     Code,
+//                     CodeBlock,
+//                     Heading,
+//                     HorizontalLine,
+//                     Image,
+//                     ImageToolbar,
+//                     ImageUpload,
+//                     Base64UploadAdapter,
+//                     Italic,
+//                     // Link, // To make link visible
+//                     List,
+//                     Markdown,
+//                     Mention,
+//                     Paragraph,
+//                     SourceEditing,
+//                     SpecialCharacters,
+//                     SpecialCharactersEssentials,
+//                     Strikethrough,
+//                     Table,
+//                     TableToolbar,
+//                     TextTransformation,
+//                     TodoList,
+//                   ],
+//                   // licenseKey: "<YOUR_LICENSE_KEY>",
+//                   mention: {
+//                     // Mention configuration
+//                   },
+//                   initialData: editorData,
+//                 }}
+//               />
+//             </div>
+//           </div>
+//           <div className='col-md-6 col-12'>
+//             <div className='editor-container-right shadow' id='target-content'>
+//               {/* <div dangerouslySetInnerHTML={{ __html: data }} /> */}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Editor;
+
 import { useRef, useState, useEffect } from "react";
 import "./Editor.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
+import html2pdf from "html2pdf.js";
 import {
   ClassicEditor,
-  Bold,
   Essentials,
+  Autoformat,
+  BlockQuote,
+  Bold,
+  CloudServices,
+  Code,
+  CodeBlock,
+  Heading,
+  HorizontalLine,
+  // Image,
+  ImageToolbar,
+  ImageUpload,
+  Base64UploadAdapter,
   Italic,
+  Link,
+  List,
+  Markdown,
   Mention,
   Paragraph,
-  Undo,
   SourceEditing,
-  Markdown,
-  Autoformat,
-  Heading,
-  HeadingEditing,
+  SpecialCharacters,
+  SpecialCharactersEssentials,
+  Strikethrough,
   Table,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
 } from "ckeditor5";
-
 import "ckeditor5/ckeditor5.css";
+
 const Editor = ({ message }) => {
   const editorRef = useRef(null);
-
   const [editorData, setEditorData] = useState(message);
-  const [data, setData] = useState("");
 
   useEffect(() => {
     const targetElement = document.getElementById("target-content");
 
-    // // Make the target element non-editable
-    if (targetElement) {
-      targetElement.setAttribute("contenteditable", "false");
-    }
-    // Helper function to update the target content
     const updateTargetContent = () => {
       const sourceElement =
         document.getElementsByClassName("ck ck-editor__main")[0];
@@ -40,27 +215,23 @@ const Editor = ({ message }) => {
       }
     };
 
-    // Initial check to wait for the editor to load
     const checkEditorLoaded = setInterval(() => {
       const sourceElement =
         document.getElementsByClassName("ck ck-editor__main")[0];
       if (sourceElement) {
-        clearInterval(checkEditorLoaded); // Stop checking once loaded
-        updateTargetContent(); // Update with initial content
+        clearInterval(checkEditorLoaded);
+        updateTargetContent();
       }
-    }, 100); // Check every 100ms
+    }, 100);
 
-    // Set up MutationObserver to watch for changes in the editor
     const observer = new MutationObserver(() => updateTargetContent());
 
-    // Observe changes if the editor has loaded
     const editorContainer =
       document.getElementsByClassName("ck ck-editor__main")[0];
     if (editorContainer) {
       observer.observe(editorContainer, { childList: true, subtree: true });
     }
 
-    // Clean up intervals and observer on unmount
     return () => {
       clearInterval(checkEditorLoaded);
       observer.disconnect();
@@ -78,8 +249,6 @@ const Editor = ({ message }) => {
                 editor={ClassicEditor}
                 data={editorData}
                 onChange={(event, editor) => {
-                  console.log("event", event);
-                  console.log("editor", editor);
                   const data = editor.getData();
                   setEditorData(data);
                 }}
@@ -95,34 +264,49 @@ const Editor = ({ message }) => {
                       "italic",
                       "SourceEditing",
                       "Heading",
+                      "|",
+                      "bulletedList",
+                      "numberedList",
                     ],
                   },
                   plugins: [
-                    Bold,
+                    ClassicEditor,
                     Essentials,
+                    Autoformat,
+                    BlockQuote,
+                    Bold,
+                    CloudServices,
+                    Code,
+                    CodeBlock,
+                    Heading,
+                    HorizontalLine,
+
+                    ImageToolbar,
+                    ImageUpload,
+                    Base64UploadAdapter,
                     Italic,
+                    Link,
+                    List,
+                    Markdown,
                     Mention,
                     Paragraph,
-                    Markdown,
-                    Table,
-                    Undo,
                     SourceEditing,
-                    Autoformat,
-                    Heading,
-                    HeadingEditing,
+                    SpecialCharacters,
+                    SpecialCharactersEssentials,
+                    Strikethrough,
+                    Table,
+                    TableToolbar,
+                    TextTransformation,
+                    TodoList,
                   ],
-                  // licenseKey: "<YOUR_LICENSE_KEY>",
-                  mention: {
-                    // Mention configuration
-                  },
                   initialData: editorData,
                 }}
               />
             </div>
           </div>
           <div className='col-md-6 col-12'>
-            <div className='editor-container-right shadow' id='target-content'>
-              {/* <div dangerouslySetInnerHTML={{ __html: data }} /> */}
+            <div className='editor-container-right  ' id='target-content'>
+              {/* Content will update here */}
             </div>
           </div>
         </div>
